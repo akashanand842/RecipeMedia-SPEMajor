7d4b39df850aaabccc2a7dbfacc95c54e35ddfbe
akashanand842 <akashanand842@gmail.com>
2023-05-06 22:13:21 +0530

    css

--- client/src/pages/CreateRecipe.js
+++ client/src/pages/CreateRecipe.js
@@ -55,62 +55,64 @@ export const CreateRecipe = () => {
   };
 
   return (
-    <div className="create-recipe">
-      <h2>Create Recipe</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={recipe.name}
-          onChange={handleChange}
-        />
-        <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          name="description"
-          value={recipe.description}
-          onChange={handleChange}
-        ></textarea>
-        <label htmlFor="ingredients">Ingredients</label>
-        {recipe.ingredients.map((ingredient, index) => (
+    // <div className="bgimg">
+      <div className="create-recipe">
+        <h2>Create Recipe</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
           <input
-            key={index}
             type="text"
-            name="ingredients"
-            value={ingredient}
-            onChange={(event) => handleIngredientChange(event, index)}
+            id="name"
+            name="name"
+            value={recipe.name}
+            onChange={handleChange}
           />
-        ))}
-        <button type="button" onClick={handleAddIngredient}>
-          Add Ingredient
-        </button>
-        <label htmlFor="instructions">Instructions</label>
-        <textarea
-          id="instructions"
-          name="instructions"
-          value={recipe.instructions}
-          onChange={handleChange}
-        ></textarea>
-        <label htmlFor="imageUrl">Image URL</label>
-        <input
-          type="text"
-          id="imageUrl"
-          name="imageUrl"
-          value={recipe.imageUrl}
-          onChange={handleChange}
-        />
-        <label htmlFor="cookingTime">Cooking Time (minutes)</label>
-        <input
-          type="number"
-          id="cookingTime"
-          name="cookingTime"
-          value={recipe.cookingTime}
-          onChange={handleChange}
-        />
-        <button type="submit">Create Recipe</button>
-      </form>
-    </div>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+            value={recipe.description}
+            onChange={handleChange}
+          ></textarea>
+          <label htmlFor="ingredients">Ingredients</label>
+          {recipe.ingredients.map((ingredient, index) => (
+            <input
+              key={index}
+              type="text"
+              name="ingredients"
+              value={ingredient}
+              onChange={(event) => handleIngredientChange(event, index)}
+            />
+          ))}
+          <button type="button" onClick={handleAddIngredient}>
+            Add Ingredient
+          </button>
+          <label htmlFor="instructions">Instructions</label>
+          <textarea
+            id="instructions"
+            name="instructions"
+            value={recipe.instructions}
+            onChange={handleChange}
+          ></textarea>
+          <label htmlFor="imageUrl">Image URL</label>
+          <input
+            type="text"
+            id="imageUrl"
+            name="imageUrl"
+            value={recipe.imageUrl}
+            onChange={handleChange}
+          />
+          <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+          <input
+            type="number"
+            id="cookingTime"
+            name="cookingTime"
+            value={recipe.cookingTime}
+            onChange={handleChange}
+          />
+          <button type="submit">Create Recipe</button>
+        </form>
+      </div>
+    // </div>
   );
 };
